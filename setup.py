c02092d15c2b0fb4b66379713003567c53eb76ef
Basil Veerman <bveerman@uvic.ca>
2016-03-14 17:28:01 -0700

    Add license specification to setuptools

--- setup.py
+++ setup.py
@@ -9,10 +9,13 @@ setup(
     version =read('VERSION.txt'),
     author = "Basil Veerman",
     author_email = "bveerman@uvic.ca",
+    license = "GPL-3.0",
+    keywords = ""
     description = ("Utility for processing CF metadata"),
     url = "http://www.pacificclimate.org/",
     packages = find_packages('.'),
     extras_require = {
         'netCDF': ['netCDF4'],
     },
+    zip_safe = True,
     )
