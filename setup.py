b2eb40c158019285dcc5d46a6ea95c675837c6bb
Basil Veerman <bveerman@uvic.ca>
2016-03-14 18:48:20 -0700

    Add additional details to setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,21 @@ setup(
     author = "Basil Veerman",
     author_email = "bveerman@uvic.ca",
     license = "GPL-3.0",
-    keywords = "",
+    keywords = "climate forecast cf cmor cmip5 cmip3",
     description = ("Utility for processing CF metadata"),
-    url = "http://www.pacificclimate.org/",
+    url = "https://github.com/pacificclimate/cfmeta",
     packages = find_packages('.'),
     extras_require = {
         'netCDF': ['netCDF4'],
     },
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
     zip_safe = True,
     )
